f7f6e7ca1d99da74b4b6e6a168f751874f991564
briapete <brian.peterson@kineticdata.com>
2022-08-29 13:57:48 -0500

    updates delete-submissions-by-query to include pageination

--- delete-submissions-by-query/delete-submissions-by-query.rb
+++ delete-submissions-by-query/delete-submissions-by-query.rb
@@ -6,67 +6,72 @@ require 'logger'
 # find the directory of this script
 PWD = File.expand_path(File.dirname(__FILE__))
 
-# load the connection configuration file
+# load the @connection @configuration file
 begin
-  config = YAML.load_file( File.join(PWD, "config.yaml") )
+  @config = YAML.load_file( File.join(PWD, "config.yaml") )
 rescue
-  raise StandardError.new "There was a problem loading the config.yaml file"
+  raise StandardError.new "There was a problem loading the @config.yaml file"
 end
 
-logger = Logger.new(STDERR)
-logger.level = Logger::INFO
-logger.formatter = proc do |severity, datetime, progname, msg|
-  date_format = datetime.utc.strftime("%Y-%m-%dT%H:%M:%S.%LZ")
-  "[#{date_format}] #{severity}: #{msg}\n"
-end
+# logger = Logger.new(STDERR)
+# logger.level = Logger::INFO
+# logger.formatter = proc do |severity, datetime, progname, msg|
+  # date_format = datetime.utc.strftime("%Y-%m-%dT%H:%M:%S.%LZ")
+  # "[#{date_format}] #{severity}: #{msg}\n"
+# end
 
 # Setup logging
 @logger = TeeLogger::TeeLogger.new(STDOUT, "#{PWD}/output.log")
-@logger.level = config["LOG_LEVEL"].to_s.downcase == "debug" ? Logger::DEBUG : Logger::INFO
+@logger.level = @config["LOG_LEVEL"].to_s.downcase == "debug" ? Logger::DEBUG : Logger::INFO
 
-# Create space connection
-conn = KineticSdk::Core.new({
-  space_server_url: config["SERVER_URL"],
-  #app_server_url: config["SPACE_URL"],
-  space_slug: config["SPACE_SLUG"],
-  username: config["SPACE_USERNAME"],
-  password: config["SPACE_PASSWORD"],
+# Create space @connection
+@conn = KineticSdk::Core.new({
+  space_server_url: @config["SERVER_URL"],
+  #app_server_url: @config["SPACE_URL"],
+  space_slug: @config["SPACE_SLUG"],
+  username: @config["SPACE_USERNAME"],
+  password: @config["SPACE_PASSWORD"],
   options: {
-    log_level: config["LOG_LEVEL"],
+    log_level: @config["LOG_LEVEL"],
     log_output: "stderr"
   }
 })
 
-# Create space connection
-http_conn = KineticSdk::CustomHttp.new({
-  username: config["SPACE_USERNAME"],
-  password: config["SPACE_PASSWORD"],
+# Create space @connection
+@http_conn = KineticSdk::CustomHttp.new({
+  username: @config["SPACE_USERNAME"],
+  password: @config["SPACE_PASSWORD"],
   options: {
-	log_level: config["LOG_LEVEL"],
+	log_level: @config["LOG_LEVEL"],
 	log_output: "stderr"
   }
 })
 
-parameters = {
-  "q" => (config["QUERY"] if config["QUERY"]),
+@parameters = {
+  "type" => @config["TYPE"],
+  "coreState" => @config["CORESTATE"],
+  "q" => (@config["QUERY"] if @config["QUERY"]),
   "direction" => "DESC",
-  "limit" => config["LIMIT"],
+  "limit" => @config["LIMIT"],
   "include" => "values"
 }.compact
 
-
-# Get Submissions
-if !config["FORM_SLUG"].nil?
-	response =  conn.find_form_submissions(config["KAPP_SLUG"], config["FORM_SLUG"], parameters)
-else
-	response =  conn.find_kapp_submissions(config["KAPP_SLUG"], parameters)
+def perform_query
+  # Get Submissions
+  if !@config["FORM_SLUG"].nil?
+    @response =  @conn.find_form_submissions(@config["KAPP_SLUG"], @config["FORM_SLUG"], @parameters)
+  else
+    @response =  @conn.find_kapp_submissions(@config["KAPP_SLUG"], @parameters)
+  end
 end
 
-logger.info JSON.pretty_generate(response.content['submissions'])
+# Execute the query
+perform_query
 
-
-if response.content['submissions'].length > 0
-	puts "#{response.content['submissions'].length} submissions were found on #{config["SPACE_URL"]}."
+# Check if any submissions were found
+if @response.content['submissions'].length > 0
+	# Confirm delete action with the user
+  puts "#{@response.content['submissions'].length} submissions were found on #{@config["SERVER_URL"]}."
 	puts "Would you like to DELETE them *ALL*? (Y/N)"
 	STDOUT.flush
 	case (gets.downcase.chomp)
@@ -76,12 +81,16 @@ if response.content['submissions'].length > 0
 	else
 	  abort "Exiting"
 	end
-  # Delete Submissions
-logger.info http_conn.default_headers  
-	response.content['submissions'].each{ |submission| 
-		#conn.update_submission(submission['id'], body={"values": config["VALUES"]})
-		http_conn.delete("#{config['SERVER_URL']}/app/api/v1/submissions/#{submission['id']}", http_conn.default_headers)
-	}
+  
+  # Continue to search until a nextPageToken is empty which indicates the last page of search results.
+  while @response.content['nextPageToken']
+    # Delete Submissions
+    @response.content['submissions'].each{ |submission| 
+      @http_conn.delete("#{@config['SERVER_URL']}/app/api/v1/submissions/#{submission['id']}", @http_conn.default_headers)
+    }
+    @parameters['pageToken'] = @response.content['nextPageToken']
+    perform_query
+  end
 else
 	puts "No submissions were found"
 	STDOUT.flush
