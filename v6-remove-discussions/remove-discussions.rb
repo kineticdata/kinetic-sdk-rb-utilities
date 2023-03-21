f2bb5fef7565f5b283e8be37d16da5bf53e31196
James Davies <james.davies@kineticdata.com>
2023-03-21 19:48:07 -0400

    Uncommented code that shouldn't have been commented out

--- v6-remove-discussions/remove-discussions.rb
+++ v6-remove-discussions/remove-discussions.rb
@@ -61,71 +61,71 @@ conn_task = KineticSdk::Task.new({
 
 logger.info "\n\n\n### Beginning Migration for #{SPACE_SLUG}###"
 
-# # Loop through trees that need to be modified and inspect diffs
-# logger.info "Beginning Modification of trees"
-# modify["task"]["trees"].each do |tree|
-#   tree_title = tree["current"]
-#   new_tree_filename = tree["filename"]
-#   # Fetch tree from current system
-#   current_tree = conn_task.find_tree(tree_title)
-#   if current_tree.status == 404
-#     logger.info "NOT FOUND: Tree #{tree_title} was not found and therefore not modified."
-#     break
-#   else
-#     # Import updated tree
-#     modified_tree = File.open("#{DATA_DIR}/modifications/#{new_tree_filename}", "r")
-#     import = conn_task.import_tree(modified_tree, true)
-#     # Log Outcome
-#     logger.warn "Tree: #{tree["current"]} does not match original" if import.status != 200
-#     logger.info "Trees successfully modified" if import.status == 200
-#   end
-# end
+# Loop through trees that need to be modified and inspect diffs
+logger.info "Beginning Modification of trees"
+modify["task"]["trees"].each do |tree|
+  tree_title = tree["current"]
+  new_tree_filename = tree["filename"]
+  # Fetch tree from current system
+  current_tree = conn_task.find_tree(tree_title)
+  if current_tree.status == 404
+    logger.info "NOT FOUND: Tree #{tree_title} was not found and therefore not modified."
+    break
+  else
+    # Import updated tree
+    modified_tree = File.open("#{DATA_DIR}/modifications/#{new_tree_filename}", "r")
+    import = conn_task.import_tree(modified_tree, true)
+    # Log Outcome
+    logger.warn "Tree: #{tree["current"]} does not match original" if import.status != 200
+    logger.info "Trees successfully modified" if import.status == 200
+  end
+end
 
-# # Delete Trees
-# delete["task"]["trees"].each do |tree_title|
-#   conn_task.delete_tree(tree_title)
-#   logger.info "Trees successfully deleted"
-# end
+# Delete Trees
+delete["task"]["trees"].each do |tree_title|
+  conn_task.delete_tree(tree_title)
+  logger.info "Trees successfully deleted"
+end
 
-# # Delete Routines
-# delete["task"]["routines"].each do |routine_name|
-#   conn_task.delete_tree({
-#     "source_name" => "-",
-#     "group_name" => "-",
-#     "tree_name" => routine_name,
-#   })
-#   logger.info "Routines successfully deleted"
-# end
+# Delete Routines
+delete["task"]["routines"].each do |routine_name|
+  conn_task.delete_tree({
+    "source_name" => "-",
+    "group_name" => "-",
+    "tree_name" => routine_name,
+  })
+  logger.info "Routines successfully deleted"
+end
 
-# # Delete API Handler
-# delete["task"]["handlers"].each do |handler_def_id|
-#   conn_task.delete_handler(handler_def_id)
-#   logger.info "Handlers successfully deleted"
-# end
+# Delete API Handler
+delete["task"]["handlers"].each do |handler_def_id|
+  conn_task.delete_handler(handler_def_id)
+  logger.info "Handlers successfully deleted"
+end
 
-# # Delete Task Source
-# delete["task"]["sources"].each do |source_name|
-#   conn_task.delete_source(source_name)
-#   logger.info "Sources successfully deleted"
-# end
+# Delete Task Source
+delete["task"]["sources"].each do |source_name|
+  conn_task.delete_source(source_name)
+  logger.info "Sources successfully deleted"
+end
 
-# # Delete Space Webhooks
-# delete["space"]["webhooks"].each do |name|
-#   core_space.delete_webhook_on_space(name)
-#   logger.info "Space webhooks successfully deleted"
-# end
+# Delete Space Webhooks
+delete["space"]["webhooks"].each do |name|
+  core_space.delete_webhook_on_space(name)
+  logger.info "Space webhooks successfully deleted"
+end
 
-# # Delete Space Attributes
-# delete["space"]["attributes"].each do |name|
-#   core_space.delete_space_attribute_definition(name)
-#   logger.info "Space attributes successfully deleted"
-# end
+# Delete Space Attributes
+delete["space"]["attributes"].each do |name|
+  core_space.delete_space_attribute_definition(name)
+  logger.info "Space attributes successfully deleted"
+end
 
-# # Delete Space Attributes
-# delete["teams"]["attributes"].each do |name|
-#   core_space.delete_team_attribute_definition(name)
-#   logger.info "Team attributes successfully deleted"
-# end
+# Delete Space Attributes
+delete["teams"]["attributes"].each do |name|
+  core_space.delete_team_attribute_definition(name)
+  logger.info "Team attributes successfully deleted"
+end
 
 # Cleanup Kapps
 kapp_slugs = core_space.find_kapps.content["kapps"].collect{|k| k["slug"]}
